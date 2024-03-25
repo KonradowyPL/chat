244198b4f6d831c0c0430182eccb1a1bd227c850
KonradowyPL <konrad.chrzaszczak@gmail.com>
2024-03-25 18:47:50 +0100

    fix proxy

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 function tryParse(str, exit) {
-  if (str) {
-    try {
-      let a = JSON.parse(str);
-      return a;
-    } catch {
-      console.error(`Error while parsing string: "${str}"`);
-      return exit;
-    }
+  if (!str) return exit;
+  try {
+    return JSON.parse(str);
+  } catch {
+    console.error(`Error while parsing string: "${str}"`);
+    return exit;
   }
-  return exit;
 }
 
+// behaves like normal array, but is loaded only when needed
+// args: chat id to load from localstorage
 const chatMessages = (id) => {
   return new Proxy([], {
     // target == backend object
     // property == vaule passed as key
     // value == proxy object
     get: function (target, property, value) {
-      console.log({ target, property, value });
-      if (target.length == 0) {
+      if (!target?.length) {
         target.push(...tryParse(localStorage.getItem(`CHAT:${id}`), []));
         console.info("HOT Loaded");
       }
-      if (property == "save") {
-        return () => localStorage.setItem(`CHAT:${id}`, JSON.stringify(target));
-      }
       return target[property];
+    },
+    set: function (target, property, value) {
+      if (!target?.length) this.get(target, property, value);
+      target[property] = value;
+      localStorage.setItem(`CHAT:${id}`, JSON.stringify(target));
+      return true;
     },
   });
 };
