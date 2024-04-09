15591ee7d35d22867ab32aa1b26d534270cdd5f1
KonradowyPL <konrad.chrzaszczak@gmail.com>
2024-04-09 20:19:33 +0200

    new logo, name & extension browser url

--- script.js
+++ script.js
@@ -102,3 +102,10 @@ document.addEventListener(
   },
   { passive: true }
 );
+
+// extension url basing on browser
+if (navigator.userAgent.includes("Firefox")) {
+  extension.href = "https://addons.mozilla.org/en-US/firefox/addon/access-control-allow-origin";
+} else {
+  extension.href = "https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf";
+}
